8bd601e9da1167de4fc32cf8ddf4298a80d431fd
Thiago <lucimara1422@gmail.com>
2023-08-25 16:11:08 -0300

    chore(import) rename

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
+import { Main } from '../components/Main'
+import { Hero } from '../components/Hero'
 
 export default function Home() {
   return (
@@ -14,6 +16,8 @@ export default function Home() {
         />
       </Head>
       <Header />
+      <Hero />
+      <Main />
       <Footer />
     </>
   )

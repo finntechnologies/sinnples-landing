d70e10a838e7eb447add2026166c99138b13a1f7
Thiago <lucimara1422@gmail.com>
2023-08-26 11:20:10 -0300

    chore(Container) wrong style

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -9,7 +9,7 @@ type ContainerProps = {
 export function Container({ className, ...props }: ContainerProps) {
     return (
         <div
-            className={clsx('mx-auto px-4 sm:px-6 lg:px-8 w-full overflow-x-hidden', className)}
+            className={clsx('mx-auto px-4 sm:px-6 lg:px-8 max-w-full overflow-x-hidden', className)}
             {...props}
         />
     )
